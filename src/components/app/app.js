85dc7a2d45d671ebcac6ab80135382739958d907
Pavel Baranov <pb@smarthead.ru>
2020-12-10 19:32:58 +0300

    =

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from './../header/header';
 import RandomChar from './../randomChar/randomChar';
+import ItemList from './../itemList/itemList';
+import CharDetails from './../charDetails/charDetails';
 import ErrorMessage from '../error/errorMessage';
 import CharacterPage from './../characterPage/characterPage';
+import gotService from './../../services/gotService';
+
 
 import './app.css';
 
 
 export default class App extends Component {
+    gotService = new gotService();
+    
     state = {
         showRandomChar: true,
         error: false
@@ -41,6 +47,28 @@ export default class App extends Component {
                         </Col>
                     </Row>
                     <CharacterPage/>
+                    {/* <Row>
+                        <Col md='6'>
+                            <ItemList
+                                onCharSelected={this.onCharSelected}
+                                getData={this.gotService.getAllBooks}
+                                />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails charId = {this.state.selectedChar} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList
+                                onCharSelected={this.onCharSelected}
+                                getData={this.gotService.getAllHouses}
+                                />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails charId = {this.state.selectedChar} />
+                        </Col>
+                    </Row> */}
                 </Container>
             </>
         );
--- src/components/characterPage/characterPage.js
+++ src/components/characterPage/characterPage.js
@@ -4,9 +4,11 @@ import {Col, Row} from 'reactstrap';
 import ItemList from './../itemList/itemList';
 import CharDetails from './../charDetails/charDetails';
 import ErrorMessage from '../error/errorMessage';
+import gotService from './../../services/gotService';
 
 export default class CharacterPage extends Component {
-
+    gotService = new gotService();
+    
     state = {
         selectedChar: 130,
         error: false
@@ -33,7 +35,11 @@ export default class CharacterPage extends Component {
         return (
             <Row>
                 <Col md='6'>
-                    <ItemList onCharSelected={this.onCharSelected}/>
+                    <ItemList
+                        onCharSelected={this.onCharSelected}
+                        getData={this.gotService.getAllCharacters}
+                        renderItem={(item) => `${item.name} (${item.gender})`}
+                        />
                 </Col>
                 <Col md='6'>
                     <CharDetails charId = {this.state.selectedChar} />
